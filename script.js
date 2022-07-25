ea4293adbf19ebbed1aa3caf2773262118fa71cd
kmithunraj <kmithunraj@mithunraj.com>
2022-07-25 20:37:40 +0530

    Add existing project files prior to the push to GitHub.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function navbtn() {
+  var x = document.getElementById("navbar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+      var load =document.getElementById("loadingpage");
+	  function loader()
+	  {
+	        load.style.display="none";
+	  }
