084aece875d7a6f3d4ec4633bc9dc7b46b4c80d5
kmithunraj <kmithunraj@mithunraj.com>
2022-08-16 02:12:30 +0530

    nav

--- script.js
+++ script.js
@@ -17,11 +17,87 @@ function navbtn() {
     }
   }
 
+
+
+  function home() {
+    if (document.getElementById("home") !== undefined) {
+      if (document.getElementById("home").style.display = "none") {
+        document.getElementById("home").style.display = "block";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      } else {
+        document.getElementById("home").style.display = "block";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        }
+      }
+    }
+
+
   function about() {
-    var x = document.getElementById("about");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+    if (document.getElementById("about") !== undefined) {
+      if (document.getElementById("about").style.display = "none") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "block";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      } else {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      }
+    }
+  }
+
+
+  function education() {
+    if (document.getElementById("education") !== undefined) {
+      if (document.getElementById("education").style.display = "none") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "block";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      } else {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      }
+    }
+  }
+
+
+  function achievements() {
+    if (document.getElementById("achievements") !== undefined) {
+      if (document.getElementById("achievements").style.display = "none") {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "block";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      } else {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("education").style.display = "none";
+        document.getElementById("achievements").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      }
     }
   }
