42de61fd6b368e47f4df8728e74233ac5972b88b
kmithunraj <kmithunraj@mithunraj.com>
2022-08-15 20:13:54 +0530

    new portfolio

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-
-
 function navbtn() {
-  var x = document.getElementById("navbar");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+    var x = document.getElementById("navbar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
-}
 
 
 
-      var load =document.getElementById("loadingpage");
-	  function loader()
-	  {
-	        load.style.display="none";
-	  }
+
+  function about() {
+    var x = document.getElementById("about");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
